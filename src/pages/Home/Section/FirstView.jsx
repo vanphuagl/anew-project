b1d791f7a6067fb5f3ad2d891846802ff849db21
unknown <nguyen_van_phu@allgrow-labo.jp>
2023-12-18 13:55:43 +0700

    fixed firstview

--- src/pages/Home/Section/FirstView.jsx
+++ src/pages/Home/Section/FirstView.jsx
@@ -6,11 +6,12 @@ const FirstView = ({ refHeading }) => {
       <div className='firstview__container'>
         <div className='firstview__heading'>
           <h2>
-            Make changes in our daily lives while imagining a thousand years into the future. anew is a project team
-            that pursues the possibilities to be the good ancestor from a product sustainability perspective. Having the
-            starting point from consideration of contexts such as environmental crisis, culture, and economy, we
-            research the designs and materials associated with objects. And we create and develop products with lower
-            environmental impact that will be suitable for our future society.
+            Make changes in our daily lives while imagining a thousand years into the future. <span>anew</span> is a
+            project team that pursues the possibilities to be <span>the good ancestor</span> from a{' '}
+            <span>product sustainability</span> perspective. Having the starting point from consideration of contexts
+            such as environmental crisis, culture, and economy, we research the designs and materials associated with
+            objects. And we create and develop products with <span>lower environmental impact</span> that will be
+            suitable for our future society.
           </h2>
         </div>
       </div>
